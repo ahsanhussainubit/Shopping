18765b453e4d5bc9d8966ab7411ad343789de12c
GADITEK\ahsan-hussain <ahsan.hussain@gaditek.com>
2025-01-08 18:10:41 +0500

    use .env using pydantic settings model

--- app/auth/token.py
+++ app/auth/token.py
@@ -1,30 +1,26 @@
 from datetime import datetime, timedelta, timezone
 from jose import JWTError, jwt
 from ..model.schemas import TokenData
-from dotenv import load_dotenv
-import os
+from ..model.settings import settings
 
-load_dotenv()
-
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
+    if expires_delta is None:
+        expires_delta = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + expires_delta
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
-def verify_token(token: str,credentials_exception): 
+def verify_token(token: str, credentials_exception): 
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email: str = payload["sub"]
+        email: str = payload.get("sub")
         if email is None:
             raise credentials_exception
         return TokenData(username=email)
